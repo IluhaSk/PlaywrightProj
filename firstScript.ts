08b7f5a216ccc72dc99d6d0362628abebe5a70e1
isklyarevskiy <isklyarevskiy@phoenixit.ru>
2021-02-17 15:12:13 +0300

    added tagChanger

--- firstScript.ts
+++ firstScript.ts
@@ -2,11 +2,13 @@ const {chromium} = require('playwright');
 var convert = require('./siebelparser/convert.js'); 
 const ass = require('assert');
 const fs = require('fs');
-var parser = require('fast-xml-parser');
-var _ = require('lodash');
+const parser = require('fast-xml-parser');
+const _ = require('lodash');
+const fetc = require('node-fetch');
+const https = require('https');
 
 let responceRo;
-let responceBody;
+let responseBody;
 const endPointName = 'contact_Search';
 const login = 'SKL_KM';
 const password = 'SKL_KM';
@@ -19,21 +21,50 @@ var launchOptions = {
             log: (name, severity, message, args) => console.log(`${name} ${message}`)},
     args: ['--ignore-certificate-errors']};
 
+const noCertAgent = new https.Agent({rejectUnauthorized: false});
+
 function printing(object) {
     console.log(_.keys(object));
     console.log(_.get(object, 'PropertySet'));
     console.log(object.PropertySet.result.payload.searchResult.PropertySet)
 }
 
+function tagReplacer(responseBody, tagName, newValue) {
+        //const string = '12*PropertyName5*Value9*';
+        //replacer = 'NewValue';
+        var NVLen = newValue.length; 
+        var separatIndex = responseBody.indexOf('*', responseBody.indexOf(tagName));
+        var OVLen = responseBody.substring((responseBody.indexOf(tagName) + tagName.length), responseBody.indexOf('*', responseBody.indexOf(tagName)));
+        var oldValue = responseBody.substring(separatIndex + 1, 1 +  parseInt(separatIndex) + parseInt(OVLen) );
+        var regexstring;
+        if (NVLen < 10) {
+            regexstring = '\\d\\*' + oldValue;
+        } else {
+            regexstring = '\\d\\d\\*' + oldValue;
+        }
+        var regexp = new RegExp(regexstring);
+        var newString = responseBody.replace(regexp, NVLen + '*' + newValue  );
+        return newString;
+}
+
 function responseChanger(page) {
     page.route('https://rsbwstm0si1.trosbank.trus.tsocgen:8443/**', async route => {
         if (route.request().postData() == null) {route.continue();} 
             else if (route.request().postData().indexOf(endPointName) !== -1) {
-                //await page.waitForResponse(response => response.request() == route.request()); 
-                //responceRo = await route.request().response();
+                //console.log(route.request().headers());
+                await fetc(route.request().url(), { 
+                    method: route.request().method(),
+                    body: route.request().postData(), 
+                    headers: route.request().headers(),
+                    agent: noCertAgent })
+                .then(responce =>  responseBody = responce.body.read().toString());
+                
+                //await https.request()
+                console.log(responseBody);
+                responseBody = await tagReplacer(responseBody, 'First Name', 'Вадим');                
                 await route.fulfill({
-                    body: '<?xml version="1.0" encoding="UTF-8"?><SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema"><SOAP-ENV:Body><ns:InvokeOutletXEndpoint_Output xmlns:ns="http://siebel.com/CustomUI"><ns:responseString>@0*0*0*1*0*0*1*2*6*result0*15*majorApiVersion1*23*1*4*meta0*11*userMessage0*12*errorMessage0*16*userErrorMessage0*0*0*4*auth0*2*1*7*payload0*10*validation0*14*excludedClient0*0*2*12*searchResult0*26*0*0*0*10*First Name6*ПОМОЕВ18*ATC Client Segment7*Розница2*Id8*1-NJDZ2R8*BIS Code3*R2622*ATC Client Acnt Region0*9*Last Name4*УШАТ18*Search Record Type6*Клиент6*BIS Id6*FART019*Doc Seria4*240812*ATC VIP Flag1*N13*Doc Issued By10*ДИДЛВАОИЧВ11*Branch Name10*Московский3*M/F7*Мужской16*Personal Manager2*  4*Type15*ФИЗИЧЕСКОЕ ЛИЦО17*ATC Salary Client3*Нет10*Birth Date10*01/01/199010*Doc Number6*1425488*Doc Type21*Паспорт гражданина РФ11*Middle Name0*22*ATC Potential VIP Flag1*N9*Siebel Id8*1-NJDZ2R5*Email24*whwyo8lxnz@violinss.fart12*Phone Number12*+7910410506511*Reg Address0*12*Search Level1*226*0*0*0*10*First Name6*ПОМОЕВ18*ATC Client Segment7*Розница2*Id8*1-NJDZ2R8*BIS Code3*R2622*ATC Client Acnt Region0*9*Last Name4*УШАТ18*Search Record Type6*Клиент6*BIS Id6*FART019*Doc Seria4*240812*ATC VIP Flag1*N13*Doc Issued By10*ДИДЛВАОИЧВ11*Branch Name10*Московский3*M/F7*Мужской16*Personal Manager2*  4*Type15*ФИЗИЧЕСКОЕ ЛИЦО17*ATC Salary Client3*Нет10*Birth Date10*01/01/199010*Doc Number6*1425488*Doc Type21*Паспорт гражданина РФ11*Middle Name0*22*ATC Potential VIP Flag1*N9*Siebel Id8*1-NJDZ2R5*Email24*whwyo8lxnz@violinss.fart12*Phone Number12*+7910410506511*Reg Address0*12*Search Level1*2</ns:responseString></ns:InvokeOutletXEndpoint_Output></SOAP-ENV:Body></SOAP-ENV:Envelope>'
-        });
+                            body: responseBody
+              });
       } else route.continue(); });
 };
 
@@ -46,7 +77,7 @@ function responseGetter(page) {
                 .then(content => convert(content))
                 .then(content => parser.parse(content, {attributeNamePrefix:"", ignoreAttributes : false, parseAttributeValue : true}));    
             await console.log(request.postData().split('tns')[3]);
-            await printing(responceBody);
+            //await printing(responceBody);
             
         };
     });
